f4949467668b440d530e19c7605b721af2b3c543
Karan Bammi <karanbammi123@gmail.com>
2022-11-08 20:16:34 +0530

    initial commit

---
+++ scripts/visualizations.js
@@ -0,0 +1,68 @@
+
+//We only have to change background-color and height of the sorting element.
+
+var speed=1000;
+
+inp_aspeed.addEventListener("input",vis_speed);
+
+function vis_speed()
+{
+    var array_speed=inp_aspeed.value;
+    switch(parseInt(array_speed))
+    {
+        case 1: speed=1;
+                break;
+        case 2: speed=5;
+                break;
+        case 3: speed=10;
+                break;
+        case 4: speed=20;
+                break;
+        case 5: speed=40;
+                break;
+        case 6: speed=80;
+                break;
+        case 7: speed=160;
+                break;
+        case 8: speed=320;
+                break;
+        case 9: speed=640;
+                break;
+        case 10: speed=1280;
+                break;
+        case 11: speed=2560;
+                break;
+        case 12: speed=5120;
+                break;
+        case 13: speed=10240;
+                break;
+    }
+    
+    delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+}
+
+var delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+var c_delay=0;//This is updated ov every div change so that visualization is visible.
+
+function div_update(cont,height,color)
+{
+    window.setTimeout(function(){
+        cont.style=" margin:0% " + margin_size + "%; width:" + (100/array_size-(2*margin_size)) + "%; height:" + height + "%; background-color:" + color + ";";
+    },c_delay+=delay_time);
+}
+
+function enable_buttons()
+{
+    window.setTimeout(function(){
+        for(var i=0;i<butts_algos.length;i++)
+        {
+            butts_algos[i].classList=[];
+            butts_algos[i].classList.add("butt_unselected");
+
+            butts_algos[i].disabled=false;
+            inp_as.disabled=false;
+            inp_gen.disabled=false;
+            inp_aspeed.disabled=false;
+        }
+    },c_delay+=delay_time);
+}
